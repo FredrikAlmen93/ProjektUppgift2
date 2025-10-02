c6d692a26df36379ae83a5967f0e8fc1eff8ba8d
FredrikAlmen93 <Fredrik.x.almen@gmail.com>
2025-10-02 10:48:53 +0200

    scannerUI-class created

---
+++ src/main/java/scannerUI.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class scannerUI implements UI {
+    private Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public String menu() {
+        System.out.println("\nMenu");
+        System.out.println("1. Add a product");
+        System.out.println("2. List all products");
+        System.out.println("3. Show information for a product");
+        System.out.println("4. Exit");
+        return prompt("Choose an option by entering the corresponding number");
+    }
+
+    @Override
+    public void info(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public String prompt(String message) {
+        System.out.print(message + ": ");
+        return scanner.nextLine();
+    }
+}
